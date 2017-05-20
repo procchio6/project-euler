e8556e7a00f901f101cc689eee4d18ee5ff3ec15
Pat Rocchio <patrocc6@gmail.com>
2017-05-20 07:56:42 -0400

    Solved multiples of 3 and 5

---
+++ multiples-3-and-5.rb
@@ -0,0 +1,14 @@
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+def multiples_3_and_5(number)
+  sum = 0
+  (0...number).each do |num|
+    if (num%3 == 0 || num%5 == 0)
+      sum += num
+    end
+  end
+  sum
+end
+
+puts multiples_3_and_5(1000)
